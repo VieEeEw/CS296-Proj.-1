1988722a0038bd23ef7a5b8ff0f64a1b76f4996b
VieEeEw <34191413+VieEeEw@users.noreply.github.com>
2019-04-24 11:41:35 -0500

    Delete clean.py

--- clean.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-if __name__ == '__main__':
-    df = pd.DataFrame([[1, 2], [3, 4]], columns=['A', 'B'])
-    df.append(pd.Series([5, 6], index=['A', 'B']), ignore_index=True)
-    print(df)
